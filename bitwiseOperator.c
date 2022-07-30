a8eba98dce3b301309231b005a9bbdfac0b559ec
Apu kumar Giri <87590236+apukumar212@users.noreply.github.com>
2022-07-30 19:29:55 +0530

    Add files via upload

---
+++ bitwiseOperator.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+system("");
+printf("%d\n", 25 & 15);
+printf("%d\n", 30 & 20);
+printf("%d\n", 45 & 90);
+printf("%d\n", 35 & 26);
+printf("%d\n", 25 | 15);
+printf("%d\n", 36 | 42);
+printf("%d\n", 87 | 95);
+printf("%d\n", 47 | 35);
+printf("%d\n", 25 ^ 22);
+printf("%d\n", 39 ^ 49);
+printf("%d\n", ~25);
+printf("%d\n", ~39);
+printf("%d\n", ~ -25);
+printf("%d\n", ~ -47);
+printf("%d\n", ~ -96);
+printf("%d\n", 25<<1);
+printf("%d\n", 25<<10);
+}
